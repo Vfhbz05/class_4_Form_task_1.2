7775fe492afd3a8c13af0ee78a6caf534dd0ac32
Maria Soboleva <marymyasnikova501@gmail.com>
2026-01-17 14:10:36 +0400

    Задание 2 готово

--- src/components/Field.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-import { FieldLayout } from './FieldLayout';
-
-export const Field = ({field, onClickToCell}) =>{
-	return (
-		<FieldLayout field = {field} onClickToCell= {onClickToCell}/>
-	)
-}
-
